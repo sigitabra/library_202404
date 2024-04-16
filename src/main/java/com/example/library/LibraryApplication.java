616a3b00a5c7e889ed3a1b303e7771533677222d
sigitabra <sigita.zutautaite@gmail.com>
2024-04-16 18:11:08 +0300

    Initital

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -18,23 +18,6 @@ public class LibraryApplication {
         SpringApplication.run(LibraryApplication.class, args);
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void runAfterStart() {
-        this.bookService.addTestLaptops();
-        boolean isRunning = true;
-        Scanner sc = new Scanner(System.in);
-        while (isRunning) {
-            System.out.println("Select action: \n A - add new books \n D - delete books by ID \n U - update book location \n LG - list books by selected genre \n CG - get number of books by selected genre published this year \n");
-            switch (sc.nextLine().toUpperCase()) {
-                case "A" -> this.bookService.addNewBooksToDb(sc);
-                case "D" -> this.bookService.deleteBookFromDb(sc);
-                case "U" -> this.bookService.updateBookLocation(sc);
-                case "LG" -> this.bookService.printAllBooksByGenre(sc);
-                case "CG" -> this.bookService.printNumberOfBooksByCurrentYearAndGenre(sc);
-                default -> isRunning = false;
-            }
-        }
-        sc.close();
-    }
+
 
 }
